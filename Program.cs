11545f0ac49b9b0698f226b6ad75491108a3f40f
Casey <dinnerdoggy@gmail.com>
2025-01-21 13:35:16 -0600

    four tries per game implemented

--- Program.cs
+++ Program.cs
@@ -5,42 +5,62 @@ while (!exit)
     Console.Clear();
 
     void StartGame()
-    {
-        Console.Write("Guess the secret number (1-100): ");
-        int userGuess = int.Parse(Console.ReadLine());
-        Console.Clear();
-        Console.WriteLine($"You guessed {userGuess}!");
-        Console.ReadLine();
-
-        Console.Clear();
-        int secretNumber = 42;
-
-        string success = "You've guessed correctly!";
-        string failure = "You've guess wrong.";
-
-        string message = userGuess == secretNumber ? success : failure;
-
-        Console.WriteLine(message);
-        Console.WriteLine("\nPlay again (y/n)?");
-
-        string choice = Console.ReadLine().Trim().ToLower();
-        
-        switch (choice)
+    {   
+        void Attempt()
         {
-            case "y":
-                Console.Clear();
-                StartGame();
-                break;
-            case "n":
-                Console.Clear();
-                Console.WriteLine("See you next time!");
-                exit = true;
-                break;
-            default:
-                Console.Clear();
-                Console.WriteLine("Invalid Input");
-                break;
+            Console.Write("Guess the secret number (1-100): ");
+            int userGuess = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine($"You guessed {userGuess}!");
+            Console.ReadLine();
+
+            Console.Clear();
+            int secretNumber = 42;
+
+            string success = "You've guessed correctly!";
+            string failure = "Wrong.";
+
+            string message = userGuess == secretNumber ? success : failure;
+
+            Console.WriteLine(message);
         }
+
+        int tries = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            Console.WriteLine($"Attempt: {++tries}");
+            Attempt();
+        }
+
+
+
+
+
+
+        // User chooses whether or not to play again.
+        void PlayAgain()
+        {
+            Console.WriteLine("\nPlay again (y/n)?");
+            string choice = Console.ReadLine().Trim().ToLower();
+            switch (choice)
+            {
+                case "y":
+                    Console.Clear();
+                    StartGame();
+                    break;
+                case "n":
+                    Console.Clear();
+                    Console.WriteLine("See you next time!");
+                    exit = true;
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
+
+        }
+        PlayAgain();
     }
 
 
